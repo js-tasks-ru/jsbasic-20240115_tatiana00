a4775eed0ca5bc1a3d51fadf4bf1f5356ca00639
Tatiana Makusheva <t.makusheva.v@gmail.com>
2024-02-17 15:57:05 +0100

    module 4 done

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,18 @@
 function highlight(table) {
-  // ваш код...
+  const trs = table.getElementsByTagName("tr");
+  for (let i = 1; i < trs.length; i++) {
+    const tr = trs[i];
+    const [tdName, tdAge, tdGender, tdStatus] = tr.getElementsByTagName("td");
+    if (tdStatus.hasAttribute("data-available")) {
+      const dataAvailable = tdStatus.getAttribute("data-available");
+      tr.classList.add(dataAvailable === "true" ? "available" : "unavailable");
+    } else {
+      tr.hidden = true;
+    }
+
+    tr.classList.add(tdGender.textContent === "m" ? "male" : "female");
+    if (parseInt(tdAge.textContent, 10) < 18) {
+      tr.style = "text-decoration: line-through";
+    }
+  }
 }
