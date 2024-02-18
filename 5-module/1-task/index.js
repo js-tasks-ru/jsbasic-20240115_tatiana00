eafbf4e10865646355da1b7a16d001964006009c
Tatiana Makusheva <t.makusheva.v@gmail.com>
2024-02-18 23:05:55 +0100

    module 5 done

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,8 @@
 function hideSelf() {
-  // ваш код...
+  const buttons = document.body.getElementsByClassName("hide-self-button");
+
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.onclick = () => (button.hidden = true);
+  }
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,43 @@
 function initCarousel() {
-  // ваш код...
+  const innerCarousel = document.querySelector(".carousel__inner");
+  const slides = document.querySelectorAll(".carousel__slide");
+  const slideWidth = slides[0].offsetWidth;
+
+  let currentIndex = 0;
+  const prevButton = document.querySelector(".carousel__arrow_left");
+  const nextButton = document.querySelector(".carousel__arrow_right");
+
+  function nextSlide() {
+    if (currentIndex < slides.length - 1) {
+      currentIndex++;
+      innerCarousel.style.transform = `translateX(-${
+        slideWidth * currentIndex
+      }px)`;
+      prevButton.style.display = "";
+    }
+    if (currentIndex === slides.length - 1) {
+      nextButton.style.display = "none";
+    }
+  }
+  function prevSlide() {
+    if (currentIndex > 0) {
+      currentIndex--;
+      innerCarousel.style.transform = `translateX(-${
+        slideWidth * currentIndex
+      }px)`;
+      nextButton.style.display = "";
+    }
+    if (currentIndex === 0) {
+      prevButton.style.display = "none";
+    }
+    if (currentIndex === slides.length - 2) {
+      nextButton.style.display = "";
+    }
+  }
+  nextButton.addEventListener("click", nextSlide);
+  prevButton.addEventListener("click", prevSlide);
+  prevButton.style.display = "none";
+  if (slides.length <= 1) {
+    nextButton.style.display = "none";
+  }
 }
